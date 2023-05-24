2c639083ccf99ad4da94b3faf4d3ff9801f420b2
Yumarx <jumarpolanco@gmail.com>
2023-05-24 18:34:53 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int input = 10;
+
+        fib(input).stream().forEach(integer -> System.out.println(integer));
+    }
+
+    public static List<Integer> fib(int input){
+        List<Integer> serie = new ArrayList<>();
+
+        for (int i = 0; i < input; i++){
+            serie.add(calcularFibonacci(i));
+        }
+
+        return serie;
+    }
+
+    public static int calcularFibonacci(int n){
+        if(n <= 1){
+            return n;
+        } else {
+            return calcularFibonacci(n -1) + calcularFibonacci(n -2);
+        }
+
+    }
+
+}
